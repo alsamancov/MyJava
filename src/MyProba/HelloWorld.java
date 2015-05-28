51253d1941a2510584605d960bf23fc8df6687c8
alsamancov <alsamancov@gmail.com>
2015-05-28 12:48:08 +0300

    my commit

---
+++ src/MyProba/HelloWorld.java
@@ -0,0 +1,10 @@
+package MyProba;
+
+/**
+ * Created by Алексей on 5/28/2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello Git!");
+    }
+}
