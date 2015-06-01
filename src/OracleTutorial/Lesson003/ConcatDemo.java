c2d8fd593407f73244e72321d05a81a0b952175b
alsamancov <alsamancov@gmail.com>
2015-06-01 15:27:55 +0300

    9-th commit

---
+++ src/OracleTutorial/Lesson003/ConcatDemo.java
@@ -0,0 +1,13 @@
+package OracleTutorial.Lesson003;
+
+/**
+ * Created by Саманцов on 01.06.2015.
+ */
+public class ConcatDemo {
+    public static void main(String[] args){
+        String firstString = "This is";
+        String secondString = " a concatenated string";
+        String thirdString = firstString + secondString;
+        System.out.println(thirdString);
+    }
+}
